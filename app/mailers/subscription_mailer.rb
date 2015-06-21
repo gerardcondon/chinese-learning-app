54ab330ecf61b1b52575089acfaf9737fdc9dfcd
Gerard Condon <git@gerardcondon.com>
2015-06-21 16:16:05 +0000

    Adding text mails and updating the html mails

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,6 +1,4 @@
 class SubscriptionMailer < ApplicationMailer
-  default from: "chinese-learning-app@gerardcondon.com"
-  
   def welcome_email(subscriber)
     @subscriber = subscriber
     mail(to: @subscriber.email, subject: "Welcome to Hanzi!")
