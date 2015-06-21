bffdfdf6263fa10fd10a22416e6a52749e53b7d8
Gerard Condon <git@gerardcondon.com>
2015-06-21 16:17:27 +0000

    Adding initial previews for mails

--- test/mailers/previews/subscription_mailer_preview.rb
+++ test/mailers/previews/subscription_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/subscription_mailer
 class SubscriptionMailerPreview < ActionMailer::Preview
-
+    def welcome_email
+        subscription = Subscription.new
+        subscription.name = "Bob"
+        subscription.email = "bob@example.com"
+        SubscriptionMailer.welcome_email subscription
+    end
 end
