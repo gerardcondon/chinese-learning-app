8fc623c963d0b22aec5361acc7297b54b7e3d1a4
Gerard Condon <git@gerardcondon.com>
2015-06-21 16:11:22 +0000

    Adding Rails Roadie Gem

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  #layout 'mailer'
+  include Roadie::Rails::Automatic
+  
+  default from: "chinese-learning-app@gerardcondon.com"
 end
