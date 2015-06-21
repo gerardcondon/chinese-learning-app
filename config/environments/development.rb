fd3c7fa1c956e4e2625332dd5b7792755074f2a6
Gerard Condon <git@gerardcondon.com>
2015-06-21 17:23:07 +0000

    Testing dev/prod mode on openshift

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   
   # Devise Setup
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost2', port: 3000 }
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
